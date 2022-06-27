022e7c79591ada8cb4ee06f3700def64425607f7
Samuel <kungusamuel64@gmail.com>
2022-06-27 21:42:28 +0300

    path.join

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/posts', require('./routes/post.routes'));
 app.use('/api/users', require('./routes/user.routes'));
+
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => {
